788350e880a36b171dfa15dd5d31c9ba26081763
tksk8663 <tyatya-zero_ministrum-custom@hotmail.co.jp>
2025-09-09 08:00:51 +0900

    edit graph

--- public/js/loki-hub.js
+++ public/js/loki-hub.js
@@ -6,6 +6,14 @@ if (window.location.pathname === "/loki-hub/dashboard" || window.location.pathna
     if (canvas) {
       const ctx = canvas.getContext("2d");
       if (ctx) {
+        const dateTime = new Date();
+        let labels = [];
+        for (let i = 0; i < 10; i++) {
+          const hours = String(dateTime.getHours());
+          const minutes = String(dateTime.getMinutes()).padStart(2, "0");
+          labels.push(`${hours}:${minutes}`);
+          dateTime.setHours(dateTime.getHours() - 1);
+        }
         clearInterval(waitInterval);
         window._graph = new Chart(ctx, {
           type: "bar",
