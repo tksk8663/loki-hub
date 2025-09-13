3f1b07f0f3bfe66e1cbf8a2a8e97fafbda145ad0
tksk8663 <tyatya-zero_ministrum-custom@hotmail.co.jp>
2025-09-13 10:16:33 +0900

    edit any

--- main.ts
+++ main.ts
@@ -29,167 +29,176 @@ console.log(`🔄 Server restarting at ${new Date().toLocaleDateString()} ${new 
 const clients = new Set<WebSocket>();
 
 async function handler(req: Request): Promise<Response> {
-  const url = new URL(req.url, config.baseUrl);
-  const path = url.pathname;
-  const prm = url.search.replace(/^\?/, "");
-  const tmp = path.length > 1 ? path.split(".") : [];
   try {
-    let ksk = "";
-    if (tmp.length > 0 && tmp[0]) ksk = tmp[tmp.length - 1];
-    const cTp = {
-      css: { "Content-Type": "text/css" },
-      js: { "Content-Type": "application/javascript" },
-      ico: { "Content-Type": "image/x-icon" },
-      png: { "Content-Type": "image/png" },
-    };
-    const cookie = req.headers.get("cookie");
-    const cookiePrm = getCookiePrm(cookie);
-    const { headers } = req;
-
-    if (ksk === "css" || ksk === "js") {
-      const file = await Deno.open("./public" + path);
-      return new Response(file.readable, {
-        headers: new Headers(cTp[ksk]),
-      });
-    } else if (ksk === "ico") {
-      const file = await Deno.open("./" + path);
-      return new Response(file.readable, {
-        headers: new Headers(cTp[ksk]),
-      });
-    } else if (ksk === "png") {
-      const file = await Deno.open("./public/images/" + path);
-      return new Response(file.readable, {
-        headers: new Headers(cTp[ksk]),
-      });
-    } else if (path === "" || path === "/") {
-      return await gate(req, getprm(prm));
-    } else if (path === "/home") {
-      return await home(req, getprm(prm));
-    } else if (path.startsWith("/dice")) {
-      const params = getprm(prm) ?? {};
-      const rid = path.replace("/dice", "").replace("/", "");
-      if (rid && rid !== "" && rid !== "/") params["id"] = rid;
-      return await dice(req, params);
-    } else if (path === "/new-project") {
-      return await newProject();
-    } else if (path === "/exit") {
-      const data = getprm(prm);
-      if (data?.userId) userDelete(data.userId as string);
-      return await gate(req, getprm(prm));
-    } else if (path === "/auth/check") {
-      const data = getprm(prm);
-      const user = userGet(data?.userid as string);
-      const headers = new Headers();
-      headers.append("Content-Type", "application/json");
-      if (user?.name) {
-        return new Response(JSON.stringify({ user: { id: data?.userid, name: user.name, viewName: user.viewName } }), {
-          status: 200,
-          headers,
-        });
-      } else {
-        return new Response(JSON.stringify({ user: undefined }), {
-          status: 400,
-          headers,
-        });
-      }
-    } else if (req.method === "POST" && path === "/auth/google") {
-      let rnd = randomString(64);
-      const { token } = await req.json();
-      if (!token) {
-        return new Response(JSON.stringify({ success: false, error: "No token" }), {
-          status: 400,
-          headers: { "Content-Type": "application/json" },
-        });
-      }
-      const payload = payloadFilter((await verifyJwt(token)) as unknown as userPayload);
-      const uid = userIdGetFromGoogleId(payload.sub);
-      if (uid) {
-        rnd = uid;
-      } else {
-        userAdd(rnd, payload);
-      }
-
-      const headers = new Headers();
-      headers.append("Set-Cookie", `userid=${rnd}; Path=/; SameSite=Lax`);
-      headers.append("Content-Type", "application/json");
-      // 必要に応じてセッション開始やユーザー保存など
-      return new Response(JSON.stringify({ success: true, user: rnd }), {
-        status: 200,
-        headers,
-      });
-    } else if (req.method === "POST" && path === "/send_message") {
-      const message = await req.text();
-      //log.info(JSON.stringify(prm, null, 2));
-      const hook = Deno.env.get("DISCORD_WEBHOOK_URL")!;
-
-      if (message !== "") {
-        const opt = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ content: message }),
-        };
-
-        const response = await fetch(hook, opt);
-
-        const result = await response.text();
-        if (result === "") {
-          return new Response("success", {
-            status: 200,
-          });
-        } else {
-          return new Response("failer", {
-            status: 500,
-          });
-        }
-      } else {
-        return new Response("no message", {
-          status: 200,
-        });
-      }
-    } else if (req.method === "POST" && path === "/change_name") {
-      const data = getprm(prm);
-      const userName = await req.text();
-      if (data?.userid && userName !== "") {
-        userUpdate(data.userid as string, userName);
-      }
-      return new Response("success", {
-        status: 200,
-      });
-    } else if (path === "/socket" && headers.get("upgrade")?.toLowerCase() === "websocket") {
-      const { socket, response } = Deno.upgradeWebSocket(req);
-      if (!clients.has(socket)) clients.add(socket);
-      //socket.onopen = () => console.log("WebSocket connected");
-      socket.onmessage = (event) => {
-        const data = JSON.parse(event.data ?? "{}");
-        if (data) {
-          if (data.page == "dice") {
-            diceSocket(clients, data);
-          }
-        } else {
-          console.warn("unKnown data resive...");
-          console.warn(JSON.stringify(event));
-        }
+    const url = new URL(req.url, config.baseUrl);
+    const path = url.pathname;
+    const prm = url.search.replace(/^\?/, "");
+    const tmp = path.length > 1 ? path.split(".") : [];
+    try {
+      let ksk = "";
+      if (tmp.length > 0 && tmp[0]) ksk = tmp[tmp.length - 1];
+      const cTp = {
+        css: { "Content-Type": "text/css" },
+        js: { "Content-Type": "application/javascript" },
+        ico: { "Content-Type": "image/x-icon" },
+        png: { "Content-Type": "image/png" },
       };
-      //socket.onclose = () => console.log("WebSocket closed");
-      return response;
-    } else if (path.startsWith("/loki-hub")) {
-      const path_suffix = path.replace("/loki-hub", "");
-      if (path_suffix === "" || path_suffix === "/dashboard") {
+      const cookie = req.headers.get("cookie");
+      const cookiePrm = getCookiePrm(cookie);
+      const { headers } = req;
+
+      if (ksk === "css" || ksk === "js") {
+        const file = await Deno.open("./public" + path);
+        return new Response(file.readable, {
+          headers: new Headers(cTp[ksk]),
+        });
+      } else if (ksk === "ico") {
+        const file = await Deno.open("./" + path);
+        return new Response(file.readable, {
+          headers: new Headers(cTp[ksk]),
+        });
+      } else if (ksk === "png") {
+        const file = await Deno.open("./public/images/" + path);
+        return new Response(file.readable, {
+          headers: new Headers(cTp[ksk]),
+        });
+      } else if (path === "" || path === "/") {
+        return await gate(req, getprm(prm));
+      } else if (path === "/home") {
+        return await home(req, getprm(prm));
+      } else if (path.startsWith("/dice")) {
+        const params = getprm(prm) ?? {};
+        const rid = path.replace("/dice", "").replace("/", "");
+        if (rid && rid !== "" && rid !== "/") params["id"] = rid;
+        return await dice(req, params);
+      } else if (path === "/new-project") {
+        return await newProject();
+      } else if (path === "/exit") {
         const data = getprm(prm);
         if (data?.userId) userDelete(data.userId as string);
-        return await lokiDashboard(req, getprm(prm));
+        return await gate(req, getprm(prm));
+      } else if (path === "/auth/check") {
+        const data = getprm(prm);
+        const user = userGet(data?.userid as string);
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        if (user?.name) {
+          return new Response(JSON.stringify({ user: { id: data?.userid, name: user.name, viewName: user.viewName } }), {
+            status: 200,
+            headers,
+          });
+        } else {
+          return new Response(JSON.stringify({ user: undefined }), {
+            status: 400,
+            headers,
+          });
+        }
+      } else if (req.method === "POST" && path === "/auth/google") {
+        let rnd = randomString(64);
+        const { token } = await req.json();
+        if (!token) {
+          return new Response(JSON.stringify({ success: false, error: "No token" }), {
+            status: 400,
+            headers: { "Content-Type": "application/json" },
+          });
+        }
+        const payload = payloadFilter((await verifyJwt(token)) as unknown as userPayload);
+        const uid = userIdGetFromGoogleId(payload.sub);
+        if (uid) {
+          rnd = uid;
+        } else {
+          userAdd(rnd, payload);
+        }
+
+        const headers = new Headers();
+        headers.append("Set-Cookie", `userid=${rnd}; Path=/; SameSite=Lax`);
+        headers.append("Content-Type", "application/json");
+        // 必要に応じてセッション開始やユーザー保存など
+        return new Response(JSON.stringify({ success: true, user: rnd }), {
+          status: 200,
+          headers,
+        });
+      } else if (req.method === "POST" && path === "/send_message") {
+        const message = await req.text();
+        //log.info(JSON.stringify(prm, null, 2));
+        const hook = Deno.env.get("DISCORD_WEBHOOK_URL")!;
+
+        if (message !== "") {
+          const opt = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ content: message }),
+          };
+
+          const response = await fetch(hook, opt);
+
+          const result = await response.text();
+          if (result === "") {
+            return new Response("success", {
+              status: 200,
+            });
+          } else {
+            return new Response("failer", {
+              status: 500,
+            });
+          }
+        } else {
+          return new Response("no message", {
+            status: 200,
+          });
+        }
+      } else if (req.method === "POST" && path === "/change_name") {
+        const data = getprm(prm);
+        const userName = await req.text();
+        if (data?.userid && userName !== "") {
+          userUpdate(data.userid as string, userName);
+        }
+        return new Response("success", {
+          status: 200,
+        });
+      } else if (path === "/socket" && headers.get("upgrade")?.toLowerCase() === "websocket") {
+        const { socket, response } = Deno.upgradeWebSocket(req);
+        if (!clients.has(socket)) clients.add(socket);
+        //socket.onopen = () => console.log("WebSocket connected");
+        socket.onmessage = (event) => {
+          const data = JSON.parse(event.data ?? "{}");
+          if (data) {
+            if (data.page == "dice") {
+              diceSocket(clients, data);
+            }
+          } else {
+            console.warn("unKnown data resive...");
+            console.warn(JSON.stringify(event));
+          }
+        };
+        //socket.onclose = () => console.log("WebSocket closed");
+        return response;
+      } else if (path.startsWith("/loki-hub")) {
+        const path_suffix = path.replace("/loki-hub", "");
+        if (path_suffix === "" || path_suffix === "/dashboard") {
+          const data = getprm(prm);
+          if (data?.userId) userDelete(data.userId as string);
+          return await lokiDashboard(req, getprm(prm));
+        } else {
+          return errorResponse(404);
+        }
       } else {
         return errorResponse(404);
       }
-    } else {
-      return errorResponse(404);
+    } catch (e: any) {
+      const data = getprm(prm);
+      console.error("request to: " + path);
+      console.error(JSON.stringify(data));
+      console.error(e);
+      if (String(e).includes("NotFound:")) {
+        return errorResponse(404);
+      } else {
+        return errorResponse(500);
+      }
     }
   } catch (e: any) {
-    const data = getprm(prm);
-    console.error("request to: " + path);
-    console.error(JSON.stringify(data));
     console.error(e);
     if (String(e).includes("NotFound:")) {
       return errorResponse(404);
