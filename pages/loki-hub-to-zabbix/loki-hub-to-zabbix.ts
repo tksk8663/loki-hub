044e2628305b5ee2d27c6b157e9909693b2166a4
tksk8663 <tyatya-zero_ministrum-custom@hotmail.co.jp>
2025-09-15 22:58:37 +0900

    add zabbix check function

---
+++ pages/loki-hub-to-zabbix/loki-hub-to-zabbix.ts
@@ -0,0 +1,40 @@
+import { errorResponse } from "../error.ts";
+
+export async function checkZabbixConnect(_req: Request, prm: { [key: string]: number | string }): Promise<Response> {
+  try {
+    const res = fetch(`http://${prm.ip}/zabbix/api_jsonrpc.php`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json-rpc",
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "apiinfo.version",
+        params: [],
+        id: 1,
+        auth: null,
+      }),
+    });
+
+    const data = (await res).json();
+    console.log("API Response:", data);
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    return errorResponse(500);
+  }
+
+  /*body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: "user.login",
+      params: {
+        user: user,
+        password: pw,
+      },
+      id: 1,
+      auth: null,
+    }), //*/
+}
